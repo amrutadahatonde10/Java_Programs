8ae11de44d52efb7dda1412242faa017eba9b3fb
amrutadahatonde10 <amrutadahatonde10@gmail.com>
2025-08-15 10:04:02 +0530

    15-08-2025

--- Palindrome1.java
+++ Palindrome1.java
@@ -21,7 +21,7 @@ class Palindrome1 {
 
 // Main Method
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);     
         System.out.println("Enter input:");
         String ans = sc.nextLine();
 
