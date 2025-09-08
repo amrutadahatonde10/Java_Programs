0bcf55ecbf0e8d4cd3c6e53032b1c6b288c21ac8
Amruta Dahatonde <amrutadahatonde10@gmail.com>
2025-09-08 08:26:40 +0530

    Update Arrays.java

--- Arrays.java
+++ Arrays.java
@@ -2,7 +2,7 @@
 // public class Arrays{
 //     public static void main(String[] args) {
 //         Scanner sc = new Scanner(System.in);
-//         int rows = sc.nextInt();
+//         int rows = sc.nextInt(); 
 //         int cols = sc.nextInt();
 
 //         int[][] numbers = new int[rows][cols];
