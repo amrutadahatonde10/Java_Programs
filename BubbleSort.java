973bc1ad594a603afaf8d275d445b8acbfcc905f
Amruta Dahatonde <amrutadahatonde10@gmail.com>
2025-09-09 09:03:15 +0530

    Update BubbleSort.java

--- BubbleSort.java
+++ BubbleSort.java
@@ -3,7 +3,7 @@ class BubbleSort {
     public static void printArray(int arr[]) {
         for(int i=0; i<arr.length; i++) {
             System.out.print(arr[i]+" ");
-        }
+        } 
         System.out.println();
     } 
 
