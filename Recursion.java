5282311424efdbafc008ede791608979beca780b
amrutadahatonde10 <amrutadahatonde10@gmail.com>
2025-09-12 08:31:01 +0530

    12-09-2025

--- Recursion.java
+++ Recursion.java
@@ -16,35 +16,35 @@
 
 
 
-// public class Recursion {
-//     public static void printNumb(int n) {
-//         if(n == 6) {
-//             return;
-//         }
-//         System.out.println(n);
-//         printNumb(n+1); // Recursion
-//     }
-//     public static void main(String args[]) {
-//         int n = 1;
-//         printNumb(n); // n = 5
-//     }
-// }
-// // print Numbers from 1 to 5
-
-
-
-public class Recursion{
-    public static void printSum(int i, int n, int sum){
-        if(i == n) {
-            sum += i;   
-            System.out.println(sum);
+public class Recursion {
+    public static void printNumb(int n) {
+        if(n == 6) {
             return;
         }
-        sum += i;
-        printSum(i+1, n, sum);
+        System.out.println(n);
+        printNumb(n+1); // Recursion
     }
     public static void main(String args[]) {
-        printSum(1, 5, 0);
+        int n = 1;
+        printNumb(n); // n = 5
     }
 }
-// //Print sum of first n natural numbers.
+// print Numbers from 1 to 5
+
+
+
+// public class Recursion{
+//     public static void printSum(int i, int n, int sum){
+//         if(i == n) {
+//             sum += i;   
+//             System.out.println(sum);
+//             return;
+//         }
+//         sum += i;
+//         printSum(i+1, n, sum);
+//     }
+//     public static void main(String args[]) {
+//         printSum(1, 5, 0);
+//     }
+// }
+// // //Print sum of first n natural numbers.
