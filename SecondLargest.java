acf1de53721f69999a3e38b6cf1b99c5b3c22391
amrutadahatonde10 <amrutadahatonde10@gmail.com>
2025-09-15 10:32:23 +0530

    15-09-2025

--- SecondLargest.java
+++ SecondLargest.java
@@ -1,7 +1,9 @@
     public class SecondLargest {
         public static void main(String[] args) {
             int[] arr = {2,4,1,66,34,8,10,14};
+   
+            int largest = 0;
+            int secondlargest = 0;
 
-            
         }
     }
