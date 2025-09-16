f4e1d0a31b75d941c45cd519daefa532d8ab538a
amrutadahatonde10 <amrutadahatonde10@gmail.com>
2025-09-16 09:12:10 +0530

    16-09-2025

--- SecondLargest.java
+++ SecondLargest.java
@@ -2,8 +2,24 @@
         public static void main(String[] args) {
             int[] arr = {2,4,1,66,34,8,10,14};
    
-            int largest = 0;
-            int secondlargest = 0;
+            int first = Integer.MIN_VALUE;
+            int second = Integer.MIN_VALUE;
 
+            for (int i = 0; i < arr.length; i++) 
+            {
+                int x = arr[i];
+
+                if(x > first) {
+                    second = first;
+                    first = x;
+                } else if (x > second && x < first ) {
+                    second = x;
+                }
+            }
+            if (second == Integer.MIN_VALUE) {
+            System.out.println("No second largest element found.");
+            } else {
+            System.out.println("Second largest element = " + second);
+            }
         }
     }
