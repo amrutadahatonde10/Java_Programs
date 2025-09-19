7675f7265f9e878709e467badd0fb20e7c1f3a21
amrutadahatonde10 <amrutadahatonde10@gmail.com>
2025-09-19 09:30:49 +0530

    19-09-205

---
+++ evenodd.java
@@ -0,0 +1,27 @@
+// Find the largest even number and smallest odd number in an array.
+
+public class evenodd {
+    public static void main(String[] args) {
+        int[] arr = {2,4,9,8,1,5,6};
+ 
+        int largesteven = Integer.MIN_VALUE;
+        int smallestodd = Integer.MAX_VALUE;
+
+        for(int i = 0; i < arr.length; i++){
+            int x = arr[i];
+            
+            if(x % 2 == 0 ){
+               if(x > largesteven){
+                largesteven = x;
+               }
+            }
+            else {
+                if(x < smallestodd){
+                    smallestodd = x;
+                }
+            }
+        }
+        System.out.println("largesteven numbers : "+ largesteven);
+        System.out.println("smallestodd numbers : "+ smallestodd);
+    }
+}
