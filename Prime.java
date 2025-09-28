7fcc74925a5b72de6217a1166daf4c1c6c40470f
amrutadahatonde10 <amrutadahatonde10@gmail.com>
2025-09-28 11:14:29 +0530

    28-09-2025

--- Prime.java
+++ Prime.java
@@ -1,31 +1,35 @@
+
 import java.util.Scanner;
+
 public class Prime {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("Enter a number: ");
+        System.out.println("Enter a number");
         int n = sc.nextInt();
 
         boolean isPrime = true;
 
-        if (n < 2) {
+        if(n < 2) {
             System.out.println("not a prime number");
             return;
         }
 
         for(int i = 2; i <= n/2; i++){
-            if (n % i == 0) {
+            if(n % i == 0){
                 isPrime = false;
                 break;
             }
         }
-        if(isPrime) {
-           System.out.println("prime number");
-        } else {
-            System.out.println("not a prime number");
+
+        if(isPrime){
+            System.out.println("prime number");
+         } else {
+        System.out.println("not a prime number");
         }
     }
 }
+
 
 // it's divisible by 1 and a number own itself.  
 // prime no has only 2 divisors
