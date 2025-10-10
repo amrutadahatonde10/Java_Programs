b6a0d3f2ea481cb9385e8c245225601f54085532
amrutadahatonde10 <amrutadahatonde10@gmail.com>
2025-10-10 13:40:44 +0530

    10-10-2025

--- Arrays/max_min_sameloop.java
+++ Arrays/max_min_sameloop.java
@@ -1,24 +1,23 @@
 public class max_min_sameloop {
     public static void main(String[] args) {
-        int[] arr = {3,7,2,5,3};
+        int arr[] = {2,4,1111,5,33,67,10,14};
 
-        int max = Integer.MIN_VALUE;
         int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
 
-        for(int i = 0; i < arr.length; i++) {
+        for(int i = 0; i < arr.length; i++){
             int x = arr[i];
+        
 
-            if(x > max) {
+            if (x > max) {
                 max = x;
             }
 
-            if(x < min){
-                min = x;
+            if (x < min) {
+               min = x;
             }
         }
-
-
-    System.out.println("Maximum number is = " + max);
-    System.out.println("Minimum number is = " + min);
+        System.out.println("Maximum number is = " + max);
+        System.out.println("Minimum number is = " + min);
+        }
     }
-}
