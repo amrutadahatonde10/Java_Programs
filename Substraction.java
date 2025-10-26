cd829a49fa4db3f9a90a897dc1cf9972147d797c
amrutadahatonde10 <amrutadahatonde10@gmail.com>
2025-10-26 12:00:00 +0530

    26-10-2025

---
+++ Substraction.java
@@ -0,0 +1,16 @@
+ 
+import java.util.*;
+
+public class Substraction {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter value of a:");
+        int a = sc.nextInt();
+        System.out.println("Enter value of b:");
+        int b = sc.nextInt();
+
+        int sub = a - b;
+        System.out.println("Substraction of a & b numbers is : " + sub);
+     
+    } 
+} 
